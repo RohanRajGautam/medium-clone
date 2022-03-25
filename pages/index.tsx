ab4c33b5eabc3559c9dc8bbd887128b802ccf58e
Rohan Gautam <rohanrajgautam@gmail.com>
2022-03-25 13:17:51 +0545

    Add posts section and styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import { sanityClient, urlFor } from '../sanity'
 import Header from '../components/Header'
+import { Post } from '../typings'
+import Link from 'next/link'
 
-const Home: NextPage = () => {
+interface Props {
+  posts: [Post]
+}
+
+export default function Home({ posts }: Props) {
   return (
     <div className="mx-auto max-w-7xl">
       <Head>
@@ -13,13 +19,14 @@ const Home: NextPage = () => {
 
       <Header />
 
+      {/* Hero */}
       <div className="flex items-center justify-between border-y border-black bg-yellow-400 py-10 lg:py-0">
         <div className="space-y-5 px-10">
           <h1 className="max-w-xl font-serif text-6xl">
             <span className="underline decoration-black decoration-4">
               Medium
             </span>{' '}
-            is a plae to write, read, and connect
+            is a place to write, read, and connect
           </h1>
           <h2>
             It's easy and free to post your thinking on any topic and connect
@@ -32,8 +39,57 @@ const Home: NextPage = () => {
           alt="medium-m-logo"
         />
       </div>
+
+      {/* Posts */}
+      <div className="grid grid-cols-1 gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="group cursor-pointer overflow-hidden rounded-lg border">
+              <img
+                className="h-60 w-full object-cover transition-transform duration-200 ease-in-out group-hover:scale-x-105"
+                src={urlFor(post.mainImage).url()!}
+                alt={`blog-post`}
+              />
+              <div className="flex justify-between bg-white p-5">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(post.author.image).url()!}
+                  alt=""
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
 
-export default Home
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    author -> {
+      name,
+      image,
+    },
+    description,
+    mainImage,
+    slug
+  }`
+
+  const posts = await sanityClient.fetch(query)
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}
